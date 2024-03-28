6bf14b75f687a63a988c591bfa43b3e291f33c81
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-03-28 20:26:23 +0100

    Spawn simple world

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -8,13 +8,17 @@ use bevy_rapier2d::prelude::*;
 pub struct WorldPlugin;
 
 /// World size definition
-const WORLD_HEIGHT: f32 = 800.0;
+const WORLD_HEIGHT: f32 = 600.0;
 const WORLD_TOP: f32 = WORLD_HEIGHT / 2.0;
 const WORLD_BOTTOM: f32 = -WORLD_TOP;
 // Use the golden ration here for the world size
 const WORLD_WIDTH: f32 = WORLD_HEIGHT * 1.618;
 const WORLD_RIGHT: f32 = WORLD_WIDTH / 2.0;
 const WORLD_LEFT: f32 = -WORLD_RIGHT;
+const WORLD_HEIGHT_MIDDLE: f32 = 0.0; //WORLD_BOTTOM + WORLD_HEIGHT/2.0;
+const WORLD_WIDTH_MIDDLE: f32 = 0.0;
+
+const BORDER_WIDTH: f32 = 20.0;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
@@ -23,12 +27,24 @@ impl Plugin for WorldPlugin {
 }
 
 fn setup_world(mut commands: Commands) {
+    // world borders
+    spawn_wall(&mut commands, Vec3::new(WORLD_LEFT, WORLD_HEIGHT_MIDDLE, 0.0), Vec3::new(BORDER_WIDTH, WORLD_HEIGHT, 0.0));
+    spawn_wall(&mut commands, Vec3::new(WORLD_RIGHT, WORLD_HEIGHT_MIDDLE, 0.0), Vec3::new(BORDER_WIDTH, WORLD_HEIGHT, 0.0));
+    spawn_wall(&mut commands, Vec3::new(WORLD_WIDTH_MIDDLE, WORLD_TOP, 0.0), Vec3::new(WORLD_WIDTH, BORDER_WIDTH, 0.0));
+    spawn_wall(&mut commands, Vec3::new(WORLD_WIDTH_MIDDLE, WORLD_BOTTOM, 0.0), Vec3::new(WORLD_WIDTH, BORDER_WIDTH, 0.0));
+
     // Platform
+    spawn_wall(&mut commands, Vec3::new(WORLD_LEFT + WORLD_WIDTH/4.0, WORLD_BOTTOM + WORLD_HEIGHT/4.0, 0.0), Vec3::new(150.0, BORDER_WIDTH, 0.0));
+    spawn_wall(&mut commands, Vec3::new(WORLD_WIDTH_MIDDLE, WORLD_BOTTOM + WORLD_HEIGHT/2.0, 0.0), Vec3::new(150.0, BORDER_WIDTH, 0.0));
+    spawn_wall(&mut commands, Vec3::new(WORLD_RIGHT - WORLD_WIDTH/4.0, WORLD_BOTTOM + WORLD_HEIGHT*3.0/4.0, 0.0), Vec3::new(150.0, BORDER_WIDTH, 0.0));
+}
+
+fn spawn_wall(commands: &mut Commands, translation: Vec3, scale: Vec3) {
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, -200.0, 0.0),
-                scale: Vec3::new(200.0, 20.9, 0.0),
+                translation: translation,
+                scale: scale,
                 ..default()
             },
             sprite: Sprite {
@@ -40,9 +56,4 @@ fn setup_world(mut commands: Commands) {
     ))
     .insert(RigidBody::Fixed)
     .insert(Collider::cuboid(0.5, 0.5));
-
-    // Platform not visible as no sprite attached
-    commands.spawn(Collider::cuboid(20.0, 10.0))
-    .insert(TransformBundle::from(Transform::from_xyz(60.0, -10.0, 0.0)))
-    .insert(Restitution::coefficient(0.3));
 }
