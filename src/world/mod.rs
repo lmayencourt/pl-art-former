744fb5f613571d260d36cbc06e048c629e0e5bfb
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-03-30 22:28:19 +0100

    Use tiles for world

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -10,7 +10,9 @@ use bevy_rapier2d::prelude::*;
 
 pub struct WorldPlugin;
 
-const TILE_SIZE: f32 = 16.0 * 2.0;
+const TILE_SIZE: f32 = 8.0;
+const TILE_SCALER: f32 = 4.0;
+const TILE_SCALED: f32 = TILE_SIZE * TILE_SCALER;
 
 /// World size definition
 const WORLD_HEIGHT: f32 = 600.0;
@@ -30,15 +32,15 @@ impl Plugin for WorldPlugin {
 
 fn debug_grid(mut gizmos: Gizmos) {
     for x in 0..512 {
-        if x % TILE_SIZE as u32 == 0 {
+        if x % TILE_SCALED as u32 == 0 {
             gizmos.line_2d(
                 Vec2::new(
-                    WORLD_LEFT - TILE_SIZE / 2.0,
-                    WORLD_TOP + TILE_SIZE / 2.0 - (x as f32),
+                    WORLD_LEFT - TILE_SCALED / 2.0,
+                    WORLD_TOP + TILE_SCALED / 2.0 - (x as f32),
                 ),
                 Vec2::new(
-                    WORLD_RIGHT - TILE_SIZE / 2.0,
-                    WORLD_TOP + TILE_SIZE / 2.0 - (x as f32),
+                    WORLD_RIGHT - TILE_SCALED / 2.0,
+                    WORLD_TOP + TILE_SCALED / 2.0 - (x as f32),
                 ),
                 Color::GRAY,
             );
@@ -46,15 +48,15 @@ fn debug_grid(mut gizmos: Gizmos) {
     }
 
     for y in 0..1024 {
-        if y % TILE_SIZE as u32 == 0 {
+        if y % TILE_SCALED as u32 == 0 {
             gizmos.line_2d(
                 Vec2::new(
-                    WORLD_LEFT - TILE_SIZE / 2.0 + (y as f32),
-                    WORLD_TOP + TILE_SIZE / 2.0,
+                    WORLD_LEFT - TILE_SCALED / 2.0 + (y as f32),
+                    WORLD_TOP + TILE_SCALED / 2.0,
                 ),
                 Vec2::new(
-                    WORLD_LEFT - TILE_SIZE / 2.0 + (y as f32),
-                    WORLD_BOTTOM - TILE_SIZE / 2.0,
+                    WORLD_LEFT - TILE_SCALED / 2.0 + (y as f32),
+                    WORLD_BOTTOM - TILE_SCALED / 2.0,
                 ),
                 Color::GRAY,
             );
@@ -62,9 +64,19 @@ fn debug_grid(mut gizmos: Gizmos) {
     }
 }
 
-fn setup_world(mut commands: Commands) {
+fn setup_world(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
     // 2d Camera
     commands.spawn(Camera2dBundle::default());
+
+    // Tile-set
+    let texture = asset_server.load("tiles.png");
+    let layout =
+        TextureAtlasLayout::from_grid(Vec2::new(TILE_SIZE, TILE_SIZE), 4, 3, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
 
     let file = File::open("assets/map.txt").expect("No world map found in assets/ folder");
     for (y, line) in BufReader::new(file).lines().enumerate() {
@@ -72,33 +84,46 @@ fn setup_world(mut commands: Commands) {
         if let Ok(line) = line {
             for (x, char) in line.chars().enumerate() {
                 let translation = Vec3::new(
-                    WORLD_LEFT + x as f32 * TILE_SIZE,
-                    WORLD_TOP - y as f32 * TILE_SIZE,
+                    WORLD_LEFT + x as f32 * TILE_SCALED,
+                    WORLD_TOP - y as f32 * TILE_SCALED,
                     0.0,
                 );
-                let scale = Vec3::new(TILE_SIZE, TILE_SIZE, 0.0);
-                if char == '#' {
-                    spawn_wall(&mut commands, translation, scale);
+                let scale = Vec3::new(TILE_SCALER, TILE_SCALER, 0.0);
+                let idx = if char == 'B' {
+                    Some(0)
+                } else if char == 'R' {
+                    Some(1)
+                } else if char == 'G' {
+                    Some(2)
+                } else if char == 'D' {
+                    Some(3)
+                } else {
+                    None
+                };
+                if let Some(idx) = idx {
+                    let atlas = TextureAtlas {
+                        layout: texture_atlas_layout.clone(),
+                        index: idx,
+                    };
+                    spawn_wall(&mut commands, translation, scale, texture.clone(), atlas);
                 }
             }
         }
     }
 }
 
-fn spawn_wall(commands: &mut Commands, translation: Vec3, scale: Vec3) {
+fn spawn_wall(commands: &mut Commands, translation: Vec3, scale: Vec3, texture: Handle<Image>, atlas: TextureAtlas) {
     commands
-        .spawn((SpriteBundle {
+        .spawn((SpriteSheetBundle {
+            texture,
+            atlas,
             transform: Transform {
                 translation: translation,
                 scale: scale,
                 ..default()
             },
-            sprite: Sprite {
-                color: Color::rgb(1.0, 1.0, 1.0),
-                ..default()
-            },
             ..default()
         },))
         .insert(RigidBody::Fixed)
-        .insert(Collider::cuboid(0.5, 0.5));
+        .insert(Collider::cuboid(TILE_SCALER, TILE_SCALER));
 }
